5c98f183f17a9c655dc64790f8e781fe1270706f
atheist31 <rkb080@gmail.com>
2023-03-31 00:03:46 +0530

    day-3

--- FrontEnd/meesho/src/App.js
+++ FrontEnd/meesho/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import LargeWithAppLinksAndSocial from "./components/Footer";
+
+import WithSubnavigation1 from "./components/Navbar1";
+import WithSubnavigation2 from "./components/Navbar2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WithSubnavigation1 />
+      <WithSubnavigation2 />
+      <LargeWithAppLinksAndSocial/>
+      
     </div>
   );
 }
