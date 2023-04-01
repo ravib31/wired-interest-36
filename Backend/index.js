b34390cf76315e18a9bfe7f28a498547b83d3142
Abhishek Singh <sabhishek900@gmail.com>
2023-04-02 01:30:54 +0530

    new routes added

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const cors = require("cors")
 const dotenv = require("dotenv");
-const { auth } = require("./auth")
+const { auth } = require("../Backend/middlewares/auth")
 const { connection } = require("./config/db");
 const { productRouter } = require("./routes/products.routes");
-const { userRouter } = require("./routes/user.routes")
+const { userRouter } = require("./routes/user.routes");
+const { CartproductRouter } = require("./routes/cartProducts.routes");
+const { orderRouter } = require("./routes/orderList.routes");
+
+
 const app = express()
+
 app.use(express.json()) 
 app.use(cors())
 app.use("/products", productRouter)
 app.use("/user", userRouter)
-//app.use(auth)
+app.use("cart", CartproductRouter)
+app.use("order" , orderRouter)
+app.use(auth)
 
 
 
