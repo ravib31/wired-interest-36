d7aa90ddfde5c5db1e0f5c4ccbb7ba556724f32d
Soaib Akhtar <98397413+soaibakhtar@users.noreply.github.com>
2023-04-02 01:29:21 +0530

    day4

--- FrontEnd/meesho/src/App.js
+++ FrontEnd/meesho/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Product from "./components/Product";
 // import AdminSidebar from "./Components/Admin/Components/AdminSidebar";
 // import SingleUser from "./Components/Admin/Pages/SingleUser";
 // import { Navbar1 } from "./components/Navbar1";
@@ -7,14 +8,15 @@ import "./App.css";
 // import WithSubnavigation2 from "./components/Navbar2";
 // import SignIn from "./components/SignIn";
 // import SignUp from "./Components/SignUp";
-import HomePage from "./components/HomePage.jsx"
+// import HomePage from "./components/HomePage.jsx"
 
 function App() {
     return (<div className="App">
         {/* <AdminSidebar /> */}
        {/* <SignUp /> */}
        {/* <SignIn /> */}
-      <HomePage/>
+      {/* <HomePage/> */}
+      {/* <Product/> */}
       
     </div>);
 }
